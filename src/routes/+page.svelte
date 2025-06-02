e3feb301ebe3d916a396d882a427bfa25a374beb
sophievanderhorst17 <sophie_vanderhorst@hotmail.com>
2025-06-02 17:05:48 +0200

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -95,7 +95,7 @@
 	height: 90vh;
 	background-color: #F8F3EE;
 	box-shadow: 2px 0 5px rgba(0,0,0,0.1);
-	z-index: 1000;	
+	z-index: 2000000;	
 	transform: translateX(-100%);
     transition: transform 0.3s ease;
 	border-radius: 15px; /* Add rounded corners */
